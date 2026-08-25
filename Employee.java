8ece65279b0d213213c3a32c62e7d39bc2575c01
Forough <f.sahafi@gmail.com>
2026-08-25 11:12:28 +0330

    Add encapsulation to Employee

--- Employee.java
+++ Employee.java
@@ -1,14 +1,32 @@
 public class Employee {
 
-    String name;
-    int employeeId;
-    double salary;
+   private String name;
+   private int employeeId;
+   private double salary;
 
     public Employee(String name, int employeeId, double salary) {
         this.name = name;
         this.employeeId = employeeId;
         this.salary = salary;
     }
+
+public String getName(){
+	return name;
+}
+
+
+public void setName(String name){
+	this.name=name;
+}
+
+public double getSalary(){
+	return salary;
+}
+
+public void setSalary(double salary){
+	this.salary=salary;
+}
+
 
     public void showInfo() {
         System.out.println("Name: " + name);
--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Employee employee = new Employee("Forough", 1001, 50000000);
+	employee.setSalary(55000000);
 
         employee.showInfo();
     }
